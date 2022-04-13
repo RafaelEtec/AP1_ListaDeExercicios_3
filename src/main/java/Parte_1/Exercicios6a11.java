d9eb10c22caf5a368d66a907e3374a67a57fddc8
raf <raf@DESKTOP-PN9HH5P>
2022-04-12 21:30:54 -0300

    Exercícios finalizados 	Try Catch incluso em cada

--- src/main/java/Parte_1/Exercicios6a11.java
+++ src/main/java/Parte_1/Exercicios6a11.java
@@ -48,7 +48,8 @@ public class Exercicios6a11 {
                 break;
             case 10:
                 System.out.println(
-                        "Implementar uma função que escreva no terminal o dobro dos números naturais de 1 até um número informado. A mensagem deve estar no formato: \"O dobro de X é Y\".\n" +
+                        "Implementar uma função que escreva no terminal o dobro dos números naturais de 1 até um número informado.\n" +
+                        "A mensagem deve estar no formato: \"O dobro de X é Y\".\n" +
                         "Ex:  (5) => O dobro de 1 é 2\n" +
                         "            O dobro de 2 é 4\n" +
                         "            O dobro de 3 é 6\n" +
@@ -58,7 +59,8 @@ public class Exercicios6a11 {
                 break;
             case 11:
                 System.out.println(
-                        "Implementar uma função que escreva no terminal a tabuada de um número informado. A mensagem deve estar no formato: \"A x B = X\".\n" +
+                        "Implementar uma função que escreva no terminal a tabuada de um número informado.\n" +
+                        "A mensagem deve estar no formato: \"A x B = X\".\n" +
                         "Ex:  (5) => 5 x 1 = 5\n" +
                         "            5 x 2 = 10\n" +
                         "            5 x 3 = 15\n" +
@@ -75,27 +77,125 @@ public class Exercicios6a11 {
     
     public static void Exercicio6() {
         Scanner ent = new Scanner(System.in);
-        
+
+        try {
+            System.out.println("Informe o valor a ser usado: ");
+            int n = ent.nextInt();
+            
+            mostraEntre0eNParDecrescente(n);
+        } catch (Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraEntre0eNParDecrescente(int n) {
+        for (int pos = n; pos >= 0; pos--) {
+            if (pos%2 == 0) {
+                System.out.print(pos+" ");
+            }
+        }
     }
 
     public static void Exercicio7() {
         Scanner ent = new Scanner(System.in);
         
+        try {
+            System.out.println("Informe o valor a ser usado: ");
+            int n = ent.nextInt();
+            
+            mostraEntre1eNImparDecrescente(n);
+        } catch (Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraEntre1eNImparDecrescente(int n) {
+        for (int pos = n; pos >= 1; pos--) {
+            if (pos%2 == 1) {
+                System.out.print(pos+" ");
+            }
+        }
     }
 
     public static void Exercicio8() {
         Scanner ent = new Scanner(System.in);
+        
+        try {
+            System.out.println("Informe o valor de início: ");
+            int nStart = ent.nextInt();
+            System.out.println("Informe o valor final: ");
+            int nEnd = ent.nextInt();
+            
+            mostraEntre2NumsDecrescente(nStart, nEnd);
+        } catch (Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraEntre2NumsDecrescente(int nStart, int nEnd) {
+        for (int pos = nEnd; pos >= nStart; pos--) {
+            System.out.print(pos+" ");
+        }
     }
 
     public static void Exercicio9() {
         Scanner ent = new Scanner(System.in);
+        
+        try {
+            System.out.println("Informe o divisor: ");
+            int nDiv = ent.nextInt();
+            System.out.println("Informe o dividendo/valor final: ");
+            int nEnd = ent.nextInt();
+            
+            mostraMultiploNde0aN(nDiv, nEnd);
+        } catch (Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraMultiploNde0aN(int nDiv, int nEnd) {
+        for (int pos = 0; pos <= nEnd; pos++) {
+            if (pos%nDiv == 0) {
+                System.out.print(pos+" ");
+            }
+        }
     }
 
     public static void Exercicio10() {
         Scanner ent = new Scanner(System.in);
+        
+        try {
+            System.out.println("Informe o valor a ser usado: ");
+            int n = ent.nextInt();
+        
+            mostraDobro1aN(n);
+        } catch (Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraDobro1aN(int n) {
+        for (int pos = 1; pos <= n; pos++) {
+            System.out.println("O dobro de "+pos+" é "+pos*2);
+        }
     }
     
     public static void Exercicio11() {
         Scanner ent = new Scanner(System.in);
+        
+        try {
+            System.out.println("Informe o valor a ser usado: ");
+            int n = ent.nextInt();
+            
+            mostraTabuadaN(n);
+        } catch(Exception ex) {
+            System.out.println("Letras não são permitidas!");
+        }
+    }
+    
+    public static void mostraTabuadaN(int n) {
+        for (int pos = 1; pos <= 10; pos++) {
+            System.out.println(n+" X "+pos+" = "+n*pos);
+        }
     }
 }
